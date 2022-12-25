1db8a92b341f9ddaa48aa0959b3979dccbfc3666
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-25 16:21:48 +0200

    LoginForm on React

--- react-registration-form/src/App.js
+++ react-registration-form/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
-import LoginFrom from './components/LoginForm'
+import LoginForm from './components/LoginForm'
 
 
 function App() {
   return (
-    <RegistrationForm />
+    <LoginForm />
   );
 }
 
--- react-registration-form/src/components/LoginForm/index.js
+++ react-registration-form/src/components/LoginForm/index.js
@@ -1,1 +1,27 @@
- 
+ import React, { Component } from 'react';
+ import styles from './LoginForm.module.css'
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div className={styles["container"]}>
+                <form className={styles["login-form"]}>
+                    <h1>LOGIN TO YOUR ACCOUNT</h1>
+                    <input className={styles["user-info"]} type="email" name="email" placeholder="Email Address" required />
+                    <input className={styles["user-info"]} type="password" name="pass" placeholder="Password" required />
+                    <div className={styles["row"]}>
+                        <div className={styles["row"]}>
+                            <input type="checkbox" name="allow-with-rules" />
+                            <p>Remember Me</p>
+                        </div>
+                        <a className={styles["link"]} href='#'>Forgot Password</a>
+                    </div>
+                    <button className={styles["create-btn"]}>LOGIN</button>
+                    <button className={styles["sing-up-btn"]}>Sing up with Google</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
